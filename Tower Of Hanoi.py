deb92e558d1c184ed4459e35b9e040396df4cea6
Deepanshu Rana <deepanshurana4500@gmail.com>
2018-06-13 18:45:10 +0530

    Tower Of Hanoi
    TOI Code in python

---
+++ Tower Of Hanoi.py
@@ -0,0 +1,11 @@
+#towerofhanoi : 3 towers (source,auxiliary and destination)
+
+'''Algorithm:
+1. Move n-1 disks from s to a tower.
+2. Move the nth disk from s to d tower.
+3. move n-1 disks from a to d tower'''
+
+
+def toh(n,s,a,d)
+
+toh(disks =4)
