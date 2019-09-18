c7ae96606aee099d22cd58e01e3e2e53d9e413b1
Deepanshu Rana <deepanshurana4500@gmail.com>
2019-09-18 19:21:22 +0530

    Update HRMinMax.py
    --Added comments for understanding.

--- HRMinMax.py
+++ HRMinMax.py
@@ -1,18 +1,20 @@
 
-newl = [1, 2, 3, 5, 1, 13, 3]
+newl = [2, 6, 1, 12]
 lit = []
 res = []
 res.append(max(newl))
 temp = []
-for i in range(2, len(newl)+1):
-    print("WINDOW:", i)
-    for j in range(len(newl)-i+1):
-        temp = list(newl[j:j+i])
-        print(temp)
-        lit.append(min(temp))
+#------------------------------------------------------------
+for i in range(2, len(newl)+1):                                         #               |  [2, 6] [6, 1] [1, 12]
+    print("WINDOW:", i)                                                      #          |  [2, 6, 1] [6, 1, 12]
+    for j in range(len(newl)-i+1):    # code for substring formation with equal length. |  [2, 6, 1, 12]
+        temp = list(newl[j:j+i])                    #(starting with length 2)           
+#------------------------------------------------------------
+        print(temp)           
+        lit.append(min(temp))         # for every window find the minimum values.
         print("----")
     print(lit)
-    print("Calculating max of lit: ", end=' ')
-    res.append(max(lit))
+    print("Calculating max of lit: ", end=' ') 
+    res.append(max(lit))                # Out of loop, and for every windowresult, put the value in resultant.
     lit.clear()
 print(res)
